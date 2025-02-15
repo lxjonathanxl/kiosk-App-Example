7c86fe7b7f81374972e2ff1497008b5f1596e0b3
lxjonathanxl <cegavlog@gmail.com>
2025-02-15 11:51:13 -0300

    whats changed: MainActivity.kt - Added binding layout capacities activity_main.xml - Added vector icons to buttons

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,27 +1,26 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
-private lateinit var button_kiosk_on: Button
-private lateinit var button_kiosk_off: Button
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        button_kiosk_on = findViewById(R.id.button_kiosk_on)
-        button_kiosk_off = findViewById(R.id.button_kiosk_off)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        button_kiosk_on.setOnClickListener {
+        binding.buttonKioskOn.setOnClickListener {
             Toast.makeText(this, R.string.button_kiosk_on, Toast.LENGTH_SHORT).show()
             startLockTask()
          }
 
-        button_kiosk_off.setOnClickListener {
+        binding.buttonKioskOff.setOnClickListener {
             Toast.makeText(this, R.string.button_kiosk_off, Toast.LENGTH_SHORT).show()
             stopLockTask()
         }
